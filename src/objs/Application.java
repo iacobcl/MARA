c0b118f5053dfd452223a9fb5fc6ea3fbb527423
Ben <BenRGH@users.noreply.github.com>
2017-01-26 20:11:19 +0000

    Added useful comments - UP780065
    Made Application class more readable.

--- src/objs/Application.java
+++ src/objs/Application.java
@@ -19,57 +19,101 @@
 
 package objs;
 
-public class Application 
+public class Application
 {
+	/*These fields are used to define the qualities of the app*/
 	private String id;
 	private String no;
-	private double rating;
+	private double rating; /*Rating is important*/
 	private double price;
 	private double norates;
-	
 
+	/**
+	*Get method for accessing the private value of 'rating'.
+	*@return The application rating
+	*/
 	public double getRating() {
 		return rating;
 	}
 
+	/**
+	*Sets the argument as the current 'rating' for this app.
+	*@param rating The new 'rating' value
+	*/
 	public void setRating(double rating) {
 		this.rating = rating;
 	}
 
+	/**
+	*Get method for the private 'price' value.
+	*@return The 'price' of the application
+	*/
 	public double getPrice() {
 		return price;
 	}
 
+	/**
+	*Set method for 'price' value.
+	*@param price Sets the new 'price' as the argument value
+	*/
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+	/**
+	*Get method for the private 'norates' value.
+	*@return The 'norates' current value
+	*/
 	public double getNorates() {
 		return norates;
 	}
 
+	/**
+	*Set method for 'norates'.
+	*@param norates The new 'norates' value
+	*/
 	public void setNorates(double norates) {
 		this.norates = norates;
 	}
 
+	/**
+	*Constructor for the Application class.
+	*Initialises the 'id' and 'no' fields as empty strings.
+	*/
 	public Application()
 	{
 		id = new String();
 		no = new String();
 	}
 
+	/**
+	*Get method for the private id variable.
+	*@return The value of 'id'
+	*/
 	public String getId() {
 		return id;
 	}
 
+	/**
+	*Set method for the 'id' variable.
+	*@param id Sets the new value of 'id' as the argument given
+	*/
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	*Get method for the private variable 'no'.
+	*@return Returns the value of 'no'
+	*/
 	public String getNo() {
 		return no;
 	}
 
+	/**
+	*Set method for the variable 'no'.
+	*@param no The new value of 'no'
+	*/
 	public void setNo(String no) {
 		this.no = no;
 	}
