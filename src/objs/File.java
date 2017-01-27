39f9f7a49bc84abde61d081659b05b3f982c4f1e
TexFromHell <nivedasteam@gmail.com>
2017-01-27 15:12:13 +0000

    Update File.java

--- src/objs/File.java
+++ src/objs/File.java
@@ -30,25 +30,37 @@ public class File
 		fileName = new String();
 		status = 0;
 	}
-	//This is the constructor for file 
+	/*
+	This is the constructor for Class file
+	*/
 	public File(String f, int s)
 	{
 		fileName = f;
 		status = s;
 	}
-	//returns file namme
+	/* 
+	returns file name
+	*/
+	
 	public String getFileName() {
 		return fileName;
 	}
-	//Sets the file name
+	
+	/* 
+	Sets the file name
+	*/
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-	//returns the value of status
+	/*
+	returns the value of status
+	*/
 	public int getStatus() {
 		return status;
 	}
-	//sets the value of status
+	/*
+	sets the value of status
+	*/
 	public void setStatus(int status) {
 		this.status = status;
 	}
