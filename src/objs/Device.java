0750d2dd44f86eec5185c9979216ba08cd2bb353
nailah <nailahakhtar@outlook.com>
2017-02-03 12:26:49 +0000

    Update Device.java
    up766186

--- src/objs/Device.java
+++ src/objs/Device.java
@@ -19,40 +19,76 @@
 
 package objs;
 
+/**
+Class Device
+**/
+
 public class Device 
 {
 	private String device;
 	private int count;
 	private double perc;
-	
+
+/**
+*Constructor for creating a device object
+*@param device
+*@param count
+*@param perc
+**/
+		
 	public Device(String device, int count, double perc) {
 		super();
 		this.device = device;
 		this.count = count;
 		this.perc = perc;
 	}
+/**
+*Return the device
+*@return device 
+
 	public String getDevice() {
 		return device;
 	}
+/**
+*Method to set a new device name
+*@param device
+**/
 	public void setDevice(String device) {
 		this.device = device;
 	}
+	
+/**
+*Method to return the count
+*@return count
+**/
 	public int getCount() {
 		return count;
 	}
+/**
+*@param count
+**/
 	public void setCount(int count) {
 		this.count = count;
 	}
-	
+/**
+*Constructor for creating a device - setting the count and perc to 0
+*Device name to an empty string
+**/
 	public Device()
 	{
 		device = new String();
 		count = 0;
 		perc = 0;
 	}
+/**
+*@return perc
+**/
 	public double getPerc() {
 		return perc;
 	}
+/**
+*@param perc
+**/
 	public void setPerc(double perc) {
 		this.perc = perc;
 	}
