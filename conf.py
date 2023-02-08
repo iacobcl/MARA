be79cf88f6a4c6c9a6ab8e82f81aa2b7775c1c9b
claudia <iacob@port.ac.uk>
2023-02-08 13:39:31 +0000

    new doc files

---
+++ conf.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+sys.path.insert(0, os.path.abspath('extensions'))
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
+
+todo_include_todos = True
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = []
+add_function_parentheses = True
+#add_module_names = True
+# A list of ignored prefixes for module index sorting.
+#modindex_common_prefix = []
+
+
+version = ''
+release = ''
+
+
+
+def setup(app):
+     from sphinx.util.texescape import tex_replacements
+     tex_replacements += [(u'♮', u'$\\natural$'),
+                          (u'ē', u'\=e'),
+                          (u'♩', u'\quarternote'),
+                          (u'↑', u'$\\uparrow$'),
+                          ]
